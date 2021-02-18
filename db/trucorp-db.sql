e39e60d6f81edbfb5c39b8659f529d63a8ff823e
root <root@localhost.localdomain>
2021-02-18 11:33:54 -0600

    capek

---
+++ db/trucorp-db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS Trucorp;
+
+USE Trucorp;
+
+CREATE TABLE users (`ID(PK)` varchar(10), `Nama` varchar(20), `Kantor(Pusat / Cabang)` varchar(20));
+
+INSERT INTO users VALUES
+('1', 'Budi', 'Jakarta'), 
+('2', 'Budu', 'Bandung'),
+ ('3', 'Budo', 'Korea Utara');
